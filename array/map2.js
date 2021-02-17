5fa4091896cd5e2a7b33b3c96cc8f44b1976b1db
Samuel <silvasmlsss.02@gmail.com>
2021-02-17 09:50:35 -0300

    Utilizando o Array.prototype

--- array/map2.js
+++ array/map2.js
@@ -1,3 +1,11 @@
+Array.prototype.map2 = function(callback){
+    let novoArray = [];
+    for(let i = 0; i < this.length; i++){
+        novoArray.push(callback(this[i], i, this))
+    }
+    return novoArray
+}
+
 const carro = [
     '{ "nome": "Borracha", "preco": 3.45 }',
     '{ "nome": "Caderno", "preco": 13.90}',
@@ -6,8 +14,8 @@ const carro = [
 ]
 
 // Retorna um Array com apenas os preços
-const paraObjeto = json => JSON.parse(json)
-const preco      = produto => produto.preco
+// const paraObjeto = json => JSON.parse(json)
+// const preco      = produto => produto.preco
 
-const resultado = carro.map(paraObjeto).map(preco)
+const resultado = carro.map2(obj => JSON.parse(obj)).map(p => p.preco)
 console.log(resultado)
