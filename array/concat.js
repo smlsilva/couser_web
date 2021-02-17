fe2761623ef89aa7d54c95411dddf3acf7a41518
Samuel <silvasmlsss.02@gmail.com>
2021-02-17 11:07:48 -0300

    concat, flatmap, imperativoVsDeclarativo, Exercicios

---
+++ array/concat.js
@@ -0,0 +1,18 @@
+const filhas = ['Maria', 'Ana']
+const filhos = ['Joao', 'Pedro']
+const todos  = filhas.concat(filhos)
+//console.log(todos)
+
+console.log([].concat([1,2],[3,4], 5, [[9,7]]))
+/*
+const valores1 = [1, 2]
+const valores2 = [3, 4]
+const matrix   = [valores1, valores2, [5,6,'Name']]
+console.log(matrix)
+console.log(matrix[0][0])
+console.log(matrix[0][1])
+console.log(matrix[1][0])
+console.log(matrix[1][1])
+console.log(matrix[2][0])
+console.log(matrix[2][1])
+console.log(matrix[2][2])*/
---
+++ array/flatMap.js
@@ -0,0 +1,39 @@
+const escola = [
+    { nome: "Turma M1",
+      alunos: [
+          {
+              nome: "Gustavo",
+              nota: 8.1
+          },{
+              nome: "Ana",
+              nota: 9.3
+          }
+        ]
+    },{ nome: "Turma M2",
+        alunos: [
+            {
+                nome: 'Rebeca',
+                nota: 8.9
+            },{
+                nome: 'Roberto',
+                nota: 7.3
+            }
+        ]
+    }
+]
+
+const getNotaAluno = a => a.nota;
+const getNotasTurma = turma => turma.alunos.map(getNotaAluno)
+
+
+const nota1 = escola.map(getNotasTurma)
+console.log(nota1)
+
+
+// Utilizando o FlatMap ( Não existe API flatMap )
+Array.prototype.flatMap = function(callback){
+    return Array.prototype.concat.apply([], this.map(callback))
+}
+
+const notas2 = escola.flatMap(getNotasTurma)
+console.log(notas2)
